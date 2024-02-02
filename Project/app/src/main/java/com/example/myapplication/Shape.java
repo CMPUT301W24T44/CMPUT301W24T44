27d391a82302075f22d33d7c3e606e674303fb52
Denis Soh <dsoh@ualberta.ca>
2024-02-01 23:03:27 -0700

    fixed conflict with purplelish blue chose blue

--- Project/app/src/main/java/com/example/myapplication/Shape.java
+++ Project/app/src/main/java/com/example/myapplication/Shape.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 public abstract  class Shape {
 
     private String color = "purplelish blue";
+    private String color = "blue";
     private int x;
     private int y;
 }
